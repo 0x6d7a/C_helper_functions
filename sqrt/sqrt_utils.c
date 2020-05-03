1842398bb34d9908a8dc28cbdbfa31ed595b5ab4
Markzzz <markzeng14@gmail.com>
2020-05-03 12:01:51 +0800

    initial commit

---
+++ sqrt/sqrt_utils.c
@@ -0,0 +1,37 @@
+/* by Jim Ulery */
+unsigned long julery_isqrt(unsigned long val) {
+    unsigned long temp, g=0, b = 0x8000, bshft = 15;
+    do {
+        if (val >= (temp = (((g << 1) + b)<<bshft--))) {
+           g += b;
+           val -= temp;
+        }
+    } while (b >>= 1);
+    return g;
+}
+
+/* by Quake3 */
+float InvSqrt(float x){
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;            // store floating-point bits in integer
+    i = 0x5f3759df - (i >> 1);    // initial guess for Newton's method
+    x = *(float*)&i;              // convert new bits into float
+    x = x*(1.5f - xhalf*x*x);     // One round of Newton's method
+    return x;
+}
+
+/* by Tristan Muntsinger*/
+unsigned int sqrt32(unsigned long n)
+{
+    unsigned int c = 0x8000;
+    unsigned int g = 0x8000;
+
+    for(;;) {
+        if(g*g > n)
+            g ^= c;
+        c >>= 1;
+        if(c == 0)
+            return g;
+        g |= c;
+    }
+}

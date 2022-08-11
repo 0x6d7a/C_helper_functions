66ab82e5c1bdfb51edbfef50a65b2a3007445f7e
Markzzz <mark.zeng@maxustech.com>
2022-08-11 10:21:32 +0800

    [FEATURE]Add read 16 bit with machine endianness check

--- data_io/read_dat.c
+++ data_io/read_dat.c
@@ -33,13 +33,59 @@ uint8_t *readData(const char *f)
     free(buf);
 }
 
+uint16_t *readData16(const char *f)
+{
+    FILE* stream = fopen(f, "r");
+
+    if(stream == NULL)
+    {
+        printf("Open failed.\n");
+        return NULL;
+    }
+
+    /* Get the file size in bytes */
+
+    fseek(stream, 0L, SEEK_END);
+    uint32_t sz = ftell(stream);
+    fseek(stream, 0L, SEEK_SET);
+
+    printf("File size %lu 16-bits.\n", sz >> 1);
+
+    /* Read dat file into buffer */
+
+    uint16_t *buf = (uint16_t *)malloc((sz >> 1) * sizeof(uint16_t)); // sz >> 1 to conver to 16-bit size
+
+    /* Read in 16-bit */
+    
+    int n = 1;
+
+    for (uint32_t i = 0; i < (sz >> 1); i++)
+    {
+        fread(buf+i, sizeof(uint16_t), 1, stream);
+        
+        // little endian if true
+        if(*(char *)&n == 1)
+            buf[i] = ((buf[i] & 0x00FF) << 8) | (buf[i] >> 8);
+    }
+    
+    return buf;
+    free(buf);
+}
+
 int main()
 {
-    uint8_t *buf = readData("1.dat");
-    
-    for (uint32_t i = 0; i < 100; i+=2)
+    uint16_t *buf = readData16("1.dat");
+
+    /* Print in 8-bit */    
+    // for (uint32_t i = 0; i < 100; i+=2)
+    // {
+    //     printf("%02X%02X\n", buf[i], buf[i+1]);
+    // }
+
+    /* Print in 16-bit */    
+    for (uint32_t i = 0; i < 100; i++)
     {
-        printf("%2X %2X\n", buf[i], buf[i+1]);
+        printf("%04X\n", buf[i]);
     }
     
     
